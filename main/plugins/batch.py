de3344bd7bc0b771548be4be126d68fc88db93c2
Yogesh7734 <168500046+Yogesh7734@users.noreply.github.com>
2024-04-30 09:44:42 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -66,7 +66,7 @@ async def _batch(event):
                 return conv.cancel()
             try:
                 value = int(_range.text)
-                if value > 100:
+                if value > 1000:
                     await conv.send_message("You can only get upto 100 files in a single batch.")
                     return conv.cancel()
             except ValueError:
@@ -79,7 +79,7 @@ async def _batch(event):
 
 async def run_batch(userbot, client, sender, link, _range):
     for i in range(_range):
-        timer = 60
+        timer = 40
         if i < 25:
             timer = 5
         if i < 50 and i > 25:
